cad3f6b7ac7ac45e0ecf05b94e2cc766f7528677
Pol Henarejos <pol.henarejos@cttc.es>
2026-02-24 15:52:42 +0100

    Improved imap authenticator consistency
    Signed-off-by: Pol Henarejos <pol.henarejos@cttc.es>

--- certbot_castle/plugins/imap.py
+++ certbot_castle/plugins/imap.py
@@ -14,7 +14,7 @@ from certbot.display import util as display_util
 from certbot_castle import challenge
 
 import imapclient, imaplib
-from smtplib import SMTP, SMTP_SSL
+from smtplib import SMTP, SMTP_SSL, SMTPServerDisconnected
 import ssl, email
 
 
@@ -37,6 +37,40 @@ class Authenticator(common.Plugin, interfaces.Authenticator, metaclass=abc.ABCMe
         elif (mode == False and self.__in_idle == True):
             self.imap.idle_done()
             self.__in_idle = False
+
+    def _connect_imap(self, context):
+        self.imap = imapclient.IMAPClient(
+            self.conf('host'),
+            port=self.conf('port'),
+            use_uid=False,
+            ssl=True if self.conf('ssl') else False,
+            ssl_context=context
+        )
+        self.imap.login(self.conf('login'), self.conf('password'))
+        self.imap.select_folder('INBOX')
+        if b'IDLE' not in self.imap.capabilities():
+            raise errors.AuthorizationError('IMAP server does not support IDLE. Cannot continue.')
+        self.__idle(True)
+
+    def _connect_smtp(self, context):
+        method = self.conf('smtp-method')
+        smtp_server = self.conf('smtp-host') if self.conf('smtp-host') else self.conf('host')
+        port = self.conf('smtp-port') if self.conf('smtp-port') else self.conf('port')
+        login = self.conf('smtp-login') if self.conf('smtp-login') else self.conf('login')
+        password = self.conf('smtp-password') if self.conf('smtp-password') else self.conf('password')
+        if (method == 'STARTTLS'):
+            port = port if port else 587
+            self.smtp = SMTP(smtp_server,port=port)
+            self.smtp.ehlo()
+            self.smtp.starttls(context=context) # Secure the connection
+            self.smtp.ehlo() # Can be omitted
+        elif (method == 'SSL'):
+            port = port if port else 465
+            self.smtp = SMTP_SSL(smtp_server,port=port,context=context)
+        else:
+            port = port if port else 25
+            self.smtp = SMTP(smtp_server,port=port)
+        self.smtp.login(login,password)
 
     def __init__(self, *args, **kwargs):
         super(Authenticator, self).__init__(*args, **kwargs)
@@ -66,37 +100,32 @@ class Authenticator(common.Plugin, interfaces.Authenticator, metaclass=abc.ABCMe
         if self.conf('no_verify_ssl'):
             context.check_hostname = False
             context.verify_mode = ssl.CERT_NONE
-        self.imap = imapclient.IMAPClient(self.conf('host'), port=self.conf('port'), use_uid=False, ssl=True if self.conf('ssl') else False, ssl_context=context)
-        self.imap.login(self.conf('login'),self.conf('password'))
-        self.imap.select_folder('INBOX')
-        if b'IDLE' not in self.imap.capabilities():
-            raise errors.AuthorizationError('IMAP server does not support IDLE. Cannot continue.')
-        self.__idle(True)
-
-        method = self.conf('smtp-method')
-        smtp_server = self.conf('smtp-host') if self.conf('smtp-host') else self.conf('host')
-        port = self.conf('smtp-port') if self.conf('smtp-port') else self.conf('port')
-        login = self.conf('smtp-login') if self.conf('smtp-login') else self.conf('login')
-        password = self.conf('smtp-password') if self.conf('smtp-password') else self.conf('password')
-        if (method == 'STARTTLS'):
-            port = port if port else 587
-            self.smtp = SMTP(smtp_server,port=port)
-            self.smtp.ehlo()
-            self.smtp.starttls(context=context) # Secure the connection
-            self.smtp.ehlo() # Can be omitted
-        elif (method == 'SSL'):
-            port = port if port else 465
-            self.smtp = SMTP_SSL(smtp_server,port=port,context=context)
-        else:
-            port = port if port else 25
-            self.smtp = SMTP(smtp_server,port=port)
-        self.smtp.login(login,password)
+        self._imap_ssl_context = context
+        self._connect_imap(context)
+        self._connect_smtp(context)
 
     def get_chall_pref(self, domain):
         # pylint: disable=unused-argument,missing-function-docstring
         return [challenge.EmailReply00]
 
     def perform(self, achalls):  # pylint: disable=missing-function-docstring
+        # Start each challenge batch with fresh connections to avoid stale sessions
+        # across consecutive renewals in a single run.
+        try:
+            self.__idle(False)
+        except Exception:
+            pass
+        try:
+            self.imap.logout()
+        except Exception:
+            pass
+        try:
+            self.smtp.quit()
+        except Exception:
+            pass
+        self.__in_idle = False
+        self._connect_imap(self._imap_ssl_context)
+        self._connect_smtp(self._imap_ssl_context)
         return [self._perform_emailreply00(achall) for achall in achalls]
 
     def _perform_emailreply00(self, achall):
@@ -106,12 +135,17 @@ class Authenticator(common.Plugin, interfaces.Authenticator, metaclass=abc.ABCMe
         display_util.notification(text,pause=False)
         sent = False
         for i in range(60):
-            idle_resp = self.imap.idle_check(timeout=1)
+            try:
+                idle_resp = self.imap.idle_check(timeout=1)
+            except (imaplib.IMAP4.abort, OSError):
+                logger.warning("IMAP IDLE connection dropped. Reconnecting...")
+                self.__in_idle = False
+                self._connect_imap(self._imap_ssl_context)
+                continue
             for msg in idle_resp:
-                self.__idle(False)
                 uid, state = msg
                 if state == b'EXISTS':
-                    self.__set_idle(False)
+                    self.__idle(False)
                     respo = self.imap.fetch(uid, ['RFC822'])
                     for message_id, data in respo.items():
                         if (b'RFC822' in data):
@@ -128,13 +162,18 @@ class Authenticator(common.Plugin, interfaces.Authenticator, metaclass=abc.ABCMe
                                 message += 'In-Reply-To: {}\n'.format(msg['Message-ID'])
                                 message += 'Subject: Re: {}\n\n'.format(msg['Subject'])
                                 message += body
-                                self.smtp.sendmail(me,to,message)
+                                try:
+                                    self.smtp.sendmail(me,to,message)
+                                except (SMTPServerDisconnected, OSError):
+                                    logger.warning("SMTP connection dropped. Reconnecting...")
+                                    self._connect_smtp(self._imap_ssl_context)
+                                    self.smtp.sendmail(me,to,message)
 
                                 self.imap.add_flags(message_id,imapclient.SEEN)
                                 self.imap.add_flags(message_id,imapclient.DELETED)
                                 display_util.notification('The ACME response has been sent successfully!',pause=False)
                                 sent = True
-                            except castle.exception.FromAddressMismatch: #email not from challenge, continue
+                            except (castle.utils.FromAddressMismatch, castle.utils.ReceiptAddressMismatch): #email not from challenge, continue
                                 continue
                             except castle.exception.Error as e:
                                 raise errors.AuthorizationError(e.message)
@@ -149,7 +188,7 @@ class Authenticator(common.Plugin, interfaces.Authenticator, metaclass=abc.ABCMe
             self.__idle(False)
             self.imap.logout()
             self.smtp.quit()
-        except imaplib.IMAP4.abort:
+        except (imaplib.IMAP4.abort, OSError):
             pass
 
     def __idle(self,on):
